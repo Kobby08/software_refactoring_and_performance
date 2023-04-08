09c3c3305377d0c83e4c3e2b5af551d4b949f9cd
Nana Quam <quamgyambrah@gmail.com>
2023-04-07 20:52:38 -0400

    update bash scripts

--- benchmark_artemis.sh
+++ benchmark_artemis.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 
 # List of commit IDs to checkout to
-# commit_ids=(
-#     # "1392cb5f0b669129b5ba54714d1d8b0b0a00a77c"
-#     # "2105479304a7efead14ecb343686bf31042f1731"
-#     # "270b383e80296fb47dba6a719ef1616ddcaab1ef"
-#     # "27cfb2d90208403cec6727dd595f9dd1380ef441"
-#     # "3a5971ec81984a091123c21fd1b9ac6e777fc7cf"
-#     # "3fd9fbe2d0b8e9e1b29c31e6472da192e12329f6"
-#     # "54752a9cedcceef9715345730eae4766be2c8458"
-#     # "583bd3602a62af0b80676c7677d81affc80fe6ef"
-#     # "68f6d8263d8c795722805f0e4d6939e7a8b9ed48"
-#     # "858a8240f97f7cd2fc3bca052ff74a97c0bf886c"
-#     "89e0c461e5128768513943e60f6e3992f066f529"
-#     "8e40b2d4f4f242271d3dfcda4f9b96d3f94cee1b"
-#     "dd052026e6c5616f1f51795ea21d26f12deaf9e7"
-#     "fafbd7e2e5953e03573088577be620828cd77bc5"
-# )
-
 commit_ids=(
     # "1392cb5f0b669129b5ba54714d1d8b0b0a00a77c"
-    # "2105479304a7efead14ecb343686bf31042f1731"
-    # "270b383e80296fb47dba6a719ef1616ddcaab1ef"
-    # "27cfb2d90208403cec6727dd595f9dd1380ef441"
-    # "3a5971ec81984a091123c21fd1b9ac6e777fc7cf"
-    # "3fd9fbe2d0b8e9e1b29c31e6472da192e12329f6"
-    # "54752a9cedcceef9715345730eae4766be2c8458"
-    # "583bd3602a62af0b80676c7677d81affc80fe6ef"
-    # # "68f6d8263d8c795722805f0e4d6939e7a8b9ed48"
-    # # "858a8240f97f7cd2fc3bca052ff74a97c0bf886c"
-    # "89e0c461e5128768513943e60f6e3992f066f529"
+    "2105479304a7efead14ecb343686bf31042f1731"
+    "270b383e80296fb47dba6a719ef1616ddcaab1ef"
+    "27cfb2d90208403cec6727dd595f9dd1380ef441"
+    "3a5971ec81984a091123c21fd1b9ac6e777fc7cf"
+    "3fd9fbe2d0b8e9e1b29c31e6472da192e12329f6"
+    "54752a9cedcceef9715345730eae4766be2c8458"
+    "583bd3602a62af0b80676c7677d81affc80fe6ef"
+    "68f6d8263d8c795722805f0e4d6939e7a8b9ed48"
+    "858a8240f97f7cd2fc3bca052ff74a97c0bf886c"
+    "89e0c461e5128768513943e60f6e3992f066f529"
     "8e40b2d4f4f242271d3dfcda4f9b96d3f94cee1b"
     "dd052026e6c5616f1f51795ea21d26f12deaf9e7"
     "fafbd7e2e5953e03573088577be620828cd77bc5"
 )
 
 # Log file for errors
-error_log="$HOME/Desktop/sr/results/error_log.txt"
+error_log="$HOME/Desktop/sr/results/artemis/error_log.txt"
 
 run_benchmark() {
     local commit_id="$1"
@@ -51,7 +34,7 @@ run_benchmark() {
     fi
 
     # Build the Java project (assuming it's a Maven project)
-    if ! mvn clean install -DskipTests; then
+    if ! mvn clean; then
         echo -e "Error: failed to build the project for commit $commit_id \n" >>"$error_log"
         # Return to the original commit
         git checkout main
@@ -89,7 +72,7 @@ run_benchmark() {
     # Run the JMH benchmark and save the results to a file
     echo -e "\n =========================== BENCHMARK RESULTS FOR $commit_id ==================================== \n" >>"$output_file"
     # Run the JMH benchmark and save the results to a file
-    if ! java -jar target/benchmark.jar >>"$output_file"; then
+    if ! java -jar -Djmh.ignoreLock=true target/benchmark.jar >>"$output_file"; then
         echo -e "Error: failed to run the benchmark for commit $commit_id \n" >>"$error_log"
         # Return to the original commit
         git checkout main
@@ -105,7 +88,7 @@ run_benchmark() {
 
 # Iterate through the commit IDs and run the benchmark
 for commit_id in "${commit_ids[@]}"; do
-    output_file="$HOME/Desktop/sr/results/${commit_id}.txt"
+    output_file="$HOME/Desktop/sr/results/artemis/${commit_id}.txt"
 
     if ! run_benchmark "$commit_id" "$output_file"; then
         echo "Error occurred while running benchmark for commit $commit_id"
